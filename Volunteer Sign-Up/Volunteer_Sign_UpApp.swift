373d93a14074d296d527849e35ce2f3ae800c603
BishopsScope <bishopsscope@csu.fullerton.edu>
2022-06-08 11:32:19 -0700

    Initial Commit

---
+++ Volunteer Sign-Up/Volunteer_Sign_UpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Volunteer_Sign_UpApp.swift
+//  Volunteer Sign-Up
+//
+//  Created by csuftitan on 6/8/22.
+//
+
+import SwiftUI
+
+@main
+struct Volunteer_Sign_UpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
