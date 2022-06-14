15e5995ce21b949bce038f6441d386be4a5647d3
kai eusebio <keusebio@csu.fullerton.edu>
2022-06-13 23:51:48 -0700

    Added EventInfoView

---
+++ Volunteer Sign-Up/EventInfoView.swift
@@ -0,0 +1,20 @@
+//
+//  EventInfoView.swift
+//  Volunteer Sign-Up
+//
+//  Created by Kai Eusebio on 6/13/22.
+//
+
+import SwiftUI
+
+struct EventInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EventInfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        EventInfoView()
+    }
+}
