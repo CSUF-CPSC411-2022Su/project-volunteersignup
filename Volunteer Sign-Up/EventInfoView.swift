be5a17c6f7110ec487bc5c224812ae2deea3e878
kai eusebio <keusebio@csu.fullerton.edu>
2022-06-23 13:32:48 -0700

    Added ZipListEventsClass Added FindByZip func to ZipListEvents Added global object ZIP_LIST_EVENTS Converted EventInfoClass to Codable and Identifiable Some changes to EventInfoView

--- Volunteer Sign-Up/EventInfoView.swift
+++ Volunteer Sign-Up/EventInfoView.swift
@@ -8,8 +8,26 @@
 import SwiftUI
 
 struct EventInfoView: View {
+    var EventName: String = "<Fake Event Name>"
+    var dateTime: String = "1234"
+    var location: String = "P. Sherman 42 Walabee Way Sydney"
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            // Back and Event Name
+            Text("\(EventName)")
+            HStack { // 2 column
+                VStack {
+                    Text("Date :")
+                    Text("Time :")
+                    Text("Location :")
+                }
+                VStack {
+                    Text(dateTime)
+                    Text(dateTime)
+                    Text("\(location)")
+                }
+            }
+        }
     }
 }
 
