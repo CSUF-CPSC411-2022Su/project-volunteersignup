c3497af27973bcda8cdbe3219d6f49d38dd31f25
Tianna Cano <tcano@csu.fullerton.edu>
2022-06-26 20:25:13 -0700

    Update EventSearchModifier

--- Volunteer Sign-Up/EventSearchModifiers.swift
+++ Volunteer Sign-Up/EventSearchModifiers.swift
@@ -8,25 +8,6 @@
 import Foundation
 import SwiftUI
 
-struct TextEntry: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .padding(10)
-            .border(Color.black)
-            .background(Color.white)
-    }
-}
-
-struct ButtonDesign: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .padding()
-            .background(Color.black)
-            .foregroundColor(Color.white)
-            .cornerRadius(10)
-    }
-}
-
 struct Balloon: Shape {
     func path(in rect: CGRect) -> Path {
         var path = Path()
